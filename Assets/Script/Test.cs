9dd5cd00de32197f42c42ec4936acf96800cafcb
flanny7 <mottei.senshu@gmail.com>
2018-10-09 17:02:44 +0900

    first comiit

---
+++ Assets/Script/Test.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 speed = Vector3.one;
+
+    public Vector3 Speed { get { return speed * Time.deltaTime * 60; } }
+
+    private Transform trans;
+
+    private void Awake()
+    {
+        this.trans = this.transform;
+    }
+
+    private void Update()
+    {
+        this.trans.Rotate(Speed.x, Speed.y, Speed.z, Space.World);
+    }
+}
