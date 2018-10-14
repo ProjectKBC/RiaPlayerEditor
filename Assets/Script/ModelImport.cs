3594d5f75becbbe0b5cdf8a518137af8d7cb09d2
QooChaso <qoochaso238@gmail.com>
2018-10-15 00:29:50 +0900

    最低限の実装

---
+++ Assets/Script/ModelImport.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelImport : MonoBehaviour {
+
+	[SerializeField]
+    public GameObject model;
+	private GameObject insModel;
+    public Vector3 speed = Vector3.one;
+    public Vector3 Speed { get { return speed * Time.deltaTime * 60; } }
+
+    private void Start()
+    {
+        insModel = Instantiate(model);
+    }
+
+	private void Update () 
+	{
+        insModel.transform.Rotate(Speed.x, Speed.y, Speed.z, Space.World);
+	}
+}
